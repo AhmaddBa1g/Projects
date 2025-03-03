84ed30800c4462dc9b546a93aca6947576391339
AhmaddBa1g <extinguisher12@gmail.com>
2025-03-03 08:56:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-// Select the canvas and get the 2D drawing context
+
 const canvas = document.getElementById("sceneCanvas");
 const ctx = canvas.getContext("2d");
 
-// Load and draw the background image
 const background = new Image();
-background.src = "images/background.jpg"; // Ensure this path is correct
+background.src = "images/forest.jpg";
 background.onload = function () {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawForeground(); // Call function after background is drawn
+    drawForeground();
 };
 
-// Load and draw foreground images
 function drawForeground() {
     const foreground1 = new Image();
-    foreground1.src = "images/foreground1.png";
+    foreground1.src = "images/city.png";
     foreground1.onload = function () {
         ctx.drawImage(foreground1, 100, 300, 150, 150);
     };
 
     const foreground2 = new Image();
-    foreground2.src = "images/foreground2.png";
+    foreground2.src = "images/castle.png";
     foreground2.onload = function () {
         ctx.drawImage(foreground2, 500, 250, 200, 200);
     };
 
-    drawText(); // Draw text after images load
+    drawText();
 }
 
-// Add text to the canvas
 function drawText() {
     ctx.font = "24px Arial";
     ctx.fillStyle = "white";
